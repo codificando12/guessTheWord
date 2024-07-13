b9d5772623cf2250b2124165572a8a4294ee5806
codificando12 <planeta.chompi18@gmail.com>
2024-07-13 10:51:29 -0400

    game finished

--- index.js
+++ index.js
@@ -3,24 +3,39 @@ function getword () {
     const randomEntry = entries[Math.floor(Math.random() * entries.length)];
     const [randomAnimal, randomClue] = randomEntry
     clueParagraph.textContent = randomClue;
-    animal = randomAnimal
+    animal = randomAnimal;
+    lifes = 3;
+    lifesText.textContent = `You have ${lifes} left`;
+    input.value = "";
 }
 
 function checkAnswer () {
     const userInput = input.value.toLowerCase();
-    if (userInput === animal) {
-        console.log("you guess right")
-    } else {
-        console.log('try again')
-    }  
+    if (lifes > 0 ) {
+        if (userInput === animal) {
+            clueParagraph.textContent = "Correct!! Get a new clue to keep playing";
+            input.value = "";
+        } else {
+            console.log('try again');
+            input.value = "";
+            lifes --;
+            lifesText.textContent = `You have ${lifes} left. Try again`;
+            if (lifes === 0){
+                lifesText.textContent = "You LOSE! Get a new clue to keep trying"; 
+            }
+        }
+    }
 }
 
 const clueParagraph = document.querySelector("#clue");
 const input = document.querySelector("#answer-input");
 const checkButton = document.querySelector("#check-button");
 const generateWord = document.querySelector("#generate-word");
+const lifesText = document.querySelector(".lifes");
 let animal = "";
 let lifes = 3;
+
+lifesText.textContent = `You have ${lifes} left`;
 
 const guessWord = {
     "elephant": "The largest land animal with a trunk.",
@@ -33,7 +48,7 @@ const guessWord = {
     "turtle": "A reptile with a hard shell that lives both in water and on land.",
     "koala": "A tree-dwelling marsupial from Australia that feeds on eucalyptus leaves.",
     "chameleon": "A lizard known for its ability to change colors and its independently moving eyes."
-}
+};
 
 
 checkButton.addEventListener('click', checkAnswer);
